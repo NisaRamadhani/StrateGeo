9393dee5cdef7d7a947bae4e9044561b463a584a
NisaRamadhani <choirunnisanur2001@gmail.com>
2026-04-30 19:08:23 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("insight-text").innerHTML = `
-  Total Demand: ${insidePoints.length + outsidePoints.length} <br>
-  Covered: ${insidePoints.length} <br>
-  Not Covered: ${outsidePoints.length} <br><br>
-
-  There are significant uncovered areas indicating potential expansion zones.
-`;
